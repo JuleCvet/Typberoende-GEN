c593b88da694ded8192d7f490592a402f0b9e151
bahramali <bahramali.az@gmail.com>
2017-11-29 13:42:47 +0100

    initial project

---
+++ src/main/java/se/gen/genProject/App.java
@@ -0,0 +1,13 @@
+package se.gen.genProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
